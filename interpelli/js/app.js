7ffb0b16af9e531dfb5e93b82bc1d9125f4d2980
Stew <ghost.pizza@gmail.com>
2021-08-09 16:30:32 +0200

    Update app.js

--- interpelli/js/app.js
+++ interpelli/js/app.js
@@ -20,7 +20,7 @@ function pr() {
   var newContent = '';
   //  variabili per la costruzione
   newContent += '<li class="pb-3">';
-  newContent += '<a title="Apertura in nuova scheda" href="' + textUrl.trim() + url.trim() + '" target="_blank" rel="noopener" aria-label="' + title + ' (PDF, ' + size.trim() + ') - Apertura in nuova scheda">' + title.trim() + '</a>';
+  newContent += '<a title="Apertura in nuova scheda" href="' + textUrl + url.trim() + '" target="_blank" rel="noopener" aria-label="' + title + ' (PDF, ' + size.trim() + ') - Apertura in nuova scheda">' + title.trim() + '</a>';
   newContent += '<br />';
   newContent += start.trim() + '<br />';
   newContent += end.trim();
